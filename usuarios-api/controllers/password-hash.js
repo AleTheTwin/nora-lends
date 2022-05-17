5311fc8abb147ca795971d709a59e0312d6a173d
alethetwin <alethetwin@icloud.com>
2022-05-17 10:24:43 -0500

    Eliminado un console log innecesario

--- usuarios-api/controllers/password-hash.js
+++ usuarios-api/controllers/password-hash.js
@@ -2,7 +2,6 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 exports.hashPassword = (password) => {
-    console.log(password);
     return new Promise((resolve, reject) => {
         bcrypt.genSalt(saltRounds, function (err, salt) {
             bcrypt.hash(password, salt, function (err, hash) {
