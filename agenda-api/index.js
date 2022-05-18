679f33c19c9d5357e16ff27eb3cdd56067905c64
alethetwin <alethetwin@icloud.com>
2022-05-18 17:15:44 -0500

    cambios para la instalacion

--- agenda-api/index.js
+++ agenda-api/index.js
@@ -1,6 +1,6 @@
 const { app } = require("./app")
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.EVENTS_API_PORT || 8081;
 
 const server = app.listen(PORT, () => {
     console.log(`USERS API listening on port ${PORT}, http://localhost:${PORT}`);
--- install.sh
+++ install.sh
@@ -16,17 +16,17 @@ red="\033[31m"
 
 
 printf "${reset}\n"
-printf "${cyan}Installing Nora Lends Services\n"
+printf "${cyan}Instslando los servicios de Nora Lends\n"
 
 printf "${reset}\n"
-printf "${reset}Enter the usernme for the database user: default [admin]:\n"
+printf "${reset}Ingresa el nombre de usuario para el usuario de la base de datos: default [admin]:\n"
 read DB_USER
 
 if [ "$DB_USER" = "" ]; then
     DB_USER=admin
 fi
 
-printf "${reset}Enter the password for the database user: default [admin]:\n"
+printf "${reset}Ingresa la contraseña para el usuario de la base de datos: default [admin]:\n"
 read DB_PASSWORD
 
 if [ "$DB_PASSWORD" = "" ]; then
@@ -34,27 +34,42 @@ if [ "$DB_PASSWORD" = "" ]; then
 fi
 
 
-printf "${reset}Enter the port for the api users: default [8080]:\n"
-read PORT
+printf "${reset}Ingresa el puerto para la api de los usuarios: default [8080]:\n"
+read USERS_API_PORT
 
-if [ "$PORT" = "" ]; then
-    PORT=8080
+if [ "$USERS_API_PORT" = "" ]; then
+    USERS_API_PORT=8080
 fi
 
-printf "${cyan}Generating apikey for Bearer tokens decoding...\n"
+printf "${reset}Ingresa el puerto para la api de los eventos: default [8081]:\n"
+read EVENTS_API_PORT
+
+if [ "$EVENTS_API_PORT" = "" ]; then
+    EVENTS_API_PORT=8081
+fi
+
+printf "${reset}Ingresa el puerto para la api de las notas: default [8082]:\n"
+read NOTES_API_PORT
+
+if [ "$NOTES_API_PORT" = "" ]; then
+    NOTES_API_PORT=8082
+fi
+
+printf "${cyan}Generando la llave secreta para la decodificación de los tokens de acceso...\n"
 
 API_KEY=$(openssl rand -hex 20)
-printf "${green}[ ok ] ${API_KEY}\n"
+printf "${green}[ Generada ] ${cyan}${API_KEY}\n"
 
 
-printf "${cyan}Creating .env files...\n"
+printf "${cyan}Creando archivos .env...\n"
 echo "
 # Variables de entorno para la api
 
 DB_HOST=localhost
 DB_USER=$DB_USER
 DB_PASSWORD=$DB_PASSWORD
-PORT=$PORT
+USERS_API_PORT=$USERS_API_PORT
+EVENTS_API_PORT=$EVENTS_API_PORT
 API_KEY=$API_KEY
 
 
@@ -68,56 +83,58 @@ DATABASE_URL="'"postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/mydb?sche
 
 " > .env
 cp .env usuarios-api/
+cp .env agenda-api/
 
 
+printf "${reset}\n"
+printf "${cyan}Iniciando el servicio de base de datos para su configuración inicial...\n"
 docker-compose up -d --build postgres
-printf "${reset}\n"
-printf "${cyan}Waiting for the postgres service to come online...\n"
-sleep 5
 
 printf "${reset}\n"
-printf "${cyan}Installing node modules...\n"
-cd usuarios-api
+printf "${cyan}Instalando módulos de node...\n"
 
-docker run -it --rm -w /app -v $(pwd):/app node:16 npm i
+docker run -it --rm -w /app -v $(pwd)/usuarios-api/:/app node:16 npm i
+docker run -it --rm -w /app -v $(pwd)/agenda-api/:/app node:16 npm i
 
 printf "${reset}\n"
-printf "${cyan}Creating database..."
+printf "${cyan}Creando base de datos..."
 
 # npx prisma migrate dev --name init
-docker run -it --rm --network="host" -w /app -v $(pwd):/app node:16 npx prisma migrate dev --name init
+docker run -it --rm --network="host" -w /app -v $(pwd)/usuarios-api/:/app node:16 npx prisma migrate dev --name init
 
-printf "${cyan}Generating Prisma Client for docker environment...\n"
-docker run -it --rm -w /app -v $(pwd):/app node:16 npm run generate
+printf "${cyan}Generando los clientes de prisma para los ambientes de docker...\n"
+docker run -it --rm -w /app -v $(pwd)/usuarios-api/:/app node:16 npm run generate
+docker run -it --rm -w /app -v $(pwd)/agenda-api/:/app node:16 npm run generate
 
 printf "${reset}\n"
-printf "${reset}Enter the usernme for the admin user: default [admin]:\n"
+printf "${reset}Ingresa el nombre de usuario para el administrador de la api: default [admin]:\n"
 read USERNAME
 
 if [ "$USERNAME" = "" ]; then
     USERNAME=admin
 fi
 
-printf "${reset}Enter the password for the admin user: default [admin]:\n"
+printf "${reset}Ingresa la contraseña para el administrador de la api: default [admin]:\n"
 read PASSWORD
 
 if [ "$PASSWORD" = "" ]; then
     PASSWORD=admin
 fi
 
-docker run -it --rm --network="host" -w /app -v $(pwd):/app node:16 node create-user --username $USERNAME --password $PASSWORD
+docker run -it --rm --network="host" -w /app -v $(pwd)/usuarios-api:/app node:16 node create-user --username $USERNAME --password $PASSWORD
 
 printf "${reset}\n"
 
-printf "${cyan}Shutting down postgres service...\n"
+printf "${cyan}Dando de baja el servicio de base de datos...\n"
 docker-compose down
 printf "${reset}\n"
-printf "${green}Install completed"
+printf "${green}Instalación completa"
 
 printf "${reset}\n"
-printf "${reset}You may run this service with the command"
-printf "${reset}\n"
+printf "${reset}Ahora puedes correr los servicios con el comando:\n"
+echo ""
 printf "${reset}\t$ ${blue}docker-compose up -d"
 printf "${reset}\n"
+echo ""
 
 
